fd0ec8f0ae5c6dfdda31b39c2ecdbb2fa1b5f992
Vyacheslav Dolya <vyacheslav.dolya@gmail.com>
2014-07-29 17:52:46 +0300

    initialize commit

---
+++ src/Sk/DbLogger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sk;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+
+class DbLogger extends AbstractLogger implements LoggerInterface
+{
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        
+    }
+}
---
+++ src/Sk/LoggerTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sk;
+
+trait LoggerTrait
+{
+
+    protected $logger;
+
+    public function getLogger()
+    {
+        
+    }
+}
